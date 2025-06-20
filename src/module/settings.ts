f54914aac9aabb6d53ff958e9015b5e948661314
HexDrone-8850 <210758177+HexDrone-8850@users.noreply.github.com>
2025-06-19 20:10:15 -0400

    Add Dungeons & Drones protocol extension

--- src/module/settings.ts
+++ src/module/settings.ts
@@ -2,7 +2,6 @@ import { getGame } from "./utils";
 
 declare module "fvtt-types/configuration" {
   interface SettingConfig {
-    "hexprotocol.allowNarration": boolean;
     "hexprotocol.allowOOC": boolean;
     "hexprotocol.useIdentifyingAlias": boolean;
     "hexprotocol.bullyMode": boolean;
@@ -11,16 +10,6 @@ declare module "fvtt-types/configuration" {
 
 export function registerModuleSettings() {
   const { i18n, settings } = getGame();
-
-  settings.register("hexprotocol", "allowNarration", {
-    name: i18n.localize("HEXPROTO.settings.allowNarration.name"),
-    hint: i18n.localize("HEXPROTO.settings.allowNarration.hint"),
-    scope: "world",
-    config: true,
-    requiresReload: false,
-    type: Boolean,
-    default: true,
-  });
 
   settings.register("hexprotocol", "allowOOC", {
     name: i18n.localize("HEXPROTO.settings.allowOOC.name"),
